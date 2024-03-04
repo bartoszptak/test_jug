958095b062a454bff485b78ac25429b7c94f941f
Bartosz <bartptak@gmail.com>
2024-03-04 16:40:43 +0100

    0

--- run_over_repositories.py
+++ run_over_repositories.py
@@ -1,11 +1,15 @@
 import subprocess
 from glob import glob
 import shutil
+from git import Repo
+from pathlib import Path
 
 with open('repositories_list.txt', 'r') as file:
     repositories = file.readlines()
     repositories = [repo.strip() for repo in repositories]
-    
+
+Path('./repositories').mkdir(parents=True, exist_ok=True)
+Path('./_site').mkdir(parents=True, exist_ok=True)
 
 for repo in repositories:
   if len(repo) == 0:
@@ -14,24 +18,26 @@ for repo in repositories:
   repo_name = repo.split('/')[-1].split('.')[0]
   
   print(f'Cloning {repo_name}...')
-  subprocess.run(['git', 'clone', repo, repo_name])
+  Repo.clone_from(repo, f'./repositories/{repo_name}')
   
   print(f'Buiding {repo_name}...')
-  subprocess.run(['python', 'repo_name/compile.py', f'--working-directory {repo_name}'])
+  subprocess.run([f'python3', f'./repositories/{repo_name}/compile.py', f'--working-directory', f'./repositories/{repo_name}'])
   
   print(f'Create index.html for {repo_name}...')
-  files_to_index = glob(f'{repo_name}/*.html', recursive=True)
+  files_to_index = glob(f'./repositories/{repo_name}/public/*.html', recursive=True)
+  files_to_index = [f for f in files_to_index if '_pandoc' not in f]
+  files_to_index = sorted(files_to_index)
   
   txt = '<html><body>'
   for file in files_to_index:
+    file = file.split('/')[-1]
     txt += f'<a href="{file}">{file}</a><br>'
   txt += '</body></html>'
   
-  with open(f'{repo_name}/index.html', 'w') as file:
+  with open(f'./repositories/{repo_name}/public/index.html', 'w') as file:
     file.write(txt)
   
-  shutil.move(f'{repo_name}', f'./_site/{repo_name}')
-  
+  shutil.move(f'./repositories/{repo_name}/public/', f'./_site/{repo_name}')
   
 # Generate index.html for the root
 
@@ -41,7 +47,7 @@ for repo in repositories:
   if len(repo) == 0:
     continue
   repo_name = repo.split('/')[-1].split('.')[0]
-  txt += f'<a href="{repo_name}">{repo_name}</a><br>'
+  txt += f'<a href="{repo_name}/index.html">{repo_name}</a><br>'
   
 txt += '</body></html>'
 
